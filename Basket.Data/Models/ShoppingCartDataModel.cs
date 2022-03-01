92912152fcdbab6c8afd60e319dc35737d9dc54d
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2022-03-01 08:53:33 +0200

    Configure redis and create data models

---
+++ Basket.Data/Models/ShoppingCartDataModel.cs
@@ -0,0 +1,31 @@
+﻿namespace Basket.Data.Models;
+public class ShoppingCartDataModel
+{
+    public ShoppingCartDataModel()
+    {
+    }
+
+    public ShoppingCartDataModel(string username)
+    {
+        this.Username = username;
+    }
+
+    public string Username { get; set; }
+
+    public ICollection<ShoppingCartItemDataModel> ShoppingCartItems { get; set; }
+
+
+    //TODO: ServiceLayer
+    public decimal TotalPrice
+    {
+        get
+        {
+            decimal totalprice = 0;
+            foreach (var item in ShoppingCartItems)
+            {
+                totalprice += item.Price * item.Quantity;
+            }
+            return totalprice;
+        }
+    }
+}
