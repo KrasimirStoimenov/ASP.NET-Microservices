1e61ff7d3d989ce85ce4eb0a5011475cf1411f92
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2022-06-22 08:24:55 +0300

    Configure mass transit consumer

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,15 @@
+﻿namespace Ordering.API.EventBusConsumer;
+
+using System.Threading.Tasks;
+
+using EventBus.Messages.Events;
+
+using MassTransit;
+
+public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+{
+    public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+    {
+        throw new NotImplementedException();
+    }
+}
