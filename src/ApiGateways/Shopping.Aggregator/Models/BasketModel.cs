c6f901b509da2e4cfdb7c7704e52666c5feb844a
Krasimir Stoimenov <Krassimir.stoimenov@gmail.com>
2022-07-05 08:53:24 +0300

    Added Shopping.Aggregator with models

---
+++ src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Aggregator.Models;
+
+public class BasketModel
+{
+    public string Username { get; set; }
+
+    public List<BsketItemExtendedModel> Items { get; set; } = new List<BsketItemExtendedModel>();
+
+    public decimal TotalPrice { get; set; }
+}
